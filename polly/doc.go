22ef408dfe66a9036d39dc23abd3d005677eeca8
Charles Forsyth <charles.forsyth@gmail.com>
2024-04-09 10:42:13 +0100

    adjust package comment

--- polly/doc.go
+++ polly/doc.go
@@ -1,2 +1,3 @@
-// Polly provides speech.Speaker implementations for both versions of the AWS API (v1 and v2).
+// Package polly provides speech.Speaker implementations for both versions of the AWS API (v1 and v2).
+// It satisfies the [speech.Speaker] interface.
 package polly
